6116ea982edd62930ded25b40ae447044e588b26
Will Mitchell <wvmitchell@gmail.com>
2013-09-12 13:13:40 -0600

    Everyone's tweet method added

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -23,8 +23,41 @@ class MicroBlogger
       case command
         when 'q' then puts 'Goodbye!'
         when 't' then tweet parts[1..-1].join(' ')
+        when 'dm' then dm parts[1], parts[2..-1].join(' ')
+        when 'sp' then spam_my_followers
+        when 'et' then everyones_last_tweet
         else puts "Sorry I don't know how to (#{command})!"
       end
+    end
+  end
+
+  def dm(target, message)
+    valid_target(target) ? tweet("d #{target} #{message}") : (puts "Sorry, but #{target} is not one of your followers")
+  end
+
+  def valid_target(target)
+    followers_list().include?(target) ? true : false
+  end
+
+  def followers_list
+    @client.followers.collect { |follower| follower.screen_name}
+  end
+
+  def spam_my_followers
+    followers_list().each do |follower|
+      dm follower, 'SPAM FROM WILL!!!'
+    end
+  end
+
+  def everyones_last_tweet
+    friends = @client.friends
+    friends.each do |friend|
+      # find each friends's last message
+      puts friend.screen_name + " said..."
+      puts friend.status.text
+      # print each friends's screen name
+      # print each friends's message
+      puts ""
     end
   end
 end
