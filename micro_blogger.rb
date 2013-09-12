4a499b81b759427e81a3ba41ae597015e8ba6042
Will Mitchell <wvmitchell@gmail.com>
2013-09-12 11:28:57 -0600

    Iteration 1 complete

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -12,7 +12,22 @@ class MicroBlogger
   def tweet(message)
     message.length <= 140 ? @client.update(message) : (puts "Sorry, that message is too long!")
   end
+
+  def run
+    puts "Welcome to the JSL Twitter Client!"
+    command = ""
+    while command != 'q'
+      printf "enter command: "
+      parts = gets.chomp.split(' ')
+      command = parts[0]
+      case command
+        when 'q' then puts 'Goodbye!'
+        when 't' then tweet parts[1..-1].join(' ')
+        else puts "Sorry I don't know how to (#{command})!"
+      end
+    end
+  end
 end
 
 blogger = MicroBlogger.new
-blogger.tweet("MicroBlogger Initialized Initialized")
+blogger.run
